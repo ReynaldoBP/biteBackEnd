5ec3848be052d283a320f6b07166fc6f320ce5d6
ReynaldoBP <33049016+ReynaldoBP@users.noreply.github.com>
2019-08-29 17:50:43 -0500

    Web Services: Se agrega funcion de retorno de restaurante del lado movil (#19)

--- src/AppBundle/Repository/InfoRestauranteRepository.php
+++ src/AppBundle/Repository/InfoRestauranteRepository.php
@@ -103,4 +103,96 @@ class InfoRestauranteRepository extends \Doctrine\ORM\EntityRepository
         $arrayRestaurante['error'] = $strMensajeError;
         return $arrayRestaurante;
     }
+    /**
+     * Documentación para la función 'getRestauranteCriterioMovil'
+     * Método encargado de retornar todos los restaurantes según los parámetros recibidos desde movil.
+     * 
+     * @author Kevin Baque
+     * @version 1.0 29-08-2019
+     * 
+     * @return array  $arrayRestaurante
+     * 
+     */    
+    public function getRestauranteCriterioMovil($arrayParametros)
+    {
+        $strTipoComida         = $arrayParametros['strTipoComida'] ? $arrayParametros['strTipoComida']:'';
+        $strIdentificacion     = $arrayParametros['strIdentificacion'] ? $arrayParametros['strIdentificacion']:'';
+        $intIdRestaurante      = $arrayParametros['intIdRestaurante'] ? $arrayParametros['intIdRestaurante']:'';
+        $strTipoIdentificacion = $arrayParametros['strTipoIdentificacion'] ? $arrayParametros['strTipoIdentificacion']:'';
+        $strRazonSocial        = $arrayParametros['strRazonSocial'] ? $arrayParametros['strRazonSocial']:'';
+        $strEstado             = $arrayParametros['strEstado'] ? $arrayParametros['strEstado']:array('ACTIVO','INACTIVO','ELIMINADO');
+        $arrayRestaurante      = array();
+        $strMensajeError       = '';
+        $objRsmBuilder         = new ResultSetMappingBuilder($this->_em);
+        $objQuery              = $this->_em->createNativeQuery(null, $objRsmBuilder);
+        $objRsmBuilderCount    = new ResultSetMappingBuilder($this->_em);
+        $objQueryCount         = $this->_em->createNativeQuery(null, $objRsmBuilderCount);
+        try
+        {
+            $strSelect      = "SELECT IR.ID_RESTAURANTE,IR.TIPO_IDENTIFICACION, IR.IDENTIFICACION, IR.RAZON_SOCIAL,IR.USUARIO_ID, 
+                                        IR.NOMBRE_COMERCIAL, IR.REPRESENTANTE_LEGAL, IR.TIPO_COMIDA_ID,ATC.DESCRIPCION_TIPO_COMIDA, 
+                                        IR.DIRECCION_TRIBUTARIO, IR.URL_CATALOGO, IR.NUMERO_CONTACTO, IR.ESTADO ";
+            $strSelectCount = "SELECT COUNT(*) AS CANTIDAD ";
+            $strFrom        = "FROM INFO_RESTAURANTE IR,ADMI_TIPO_COMIDA ATC ";
+            $strWhere       = "WHERE IR.ESTADO in (:ESTADO) AND IR.TIPO_COMIDA_ID = ATC.ID_TIPO_COMIDA";
+            $objQuery->setParameter("ESTADO", $strEstado);
+            $objQueryCount->setParameter("ESTADO", $strEstado);
+            if(!empty($strRazonSocial))
+            {
+                $strWhere .= " AND lower(IR.RAZON_SOCIAL) like lower(:RAZON_SOCIAL)";
+                $objQuery->setParameter("RAZON_SOCIAL", '%' . trim($strRazonSocial) . '%');
+                $objQueryCount->setParameter("RAZON_SOCIAL", '%' . trim($strRazonSocial) . '%');
+            }
+            if(!empty($strTipoIdentificacion))
+            {
+                $strWhere .= " AND lower(IR.TIPO_IDENTIFICACION) like lower(:TIPO_IDENTIFICACION)";
+                $objQuery->setParameter("TIPO_IDENTIFICACION", '%' . trim($strTipoIdentificacion) . '%');
+                $objQueryCount->setParameter("TIPO_IDENTIFICACION", '%' . trim($strTipoIdentificacion) . '%');
+            }
+            if(!empty($strIdentificacion))
+            {
+                $strWhere .= " AND IR.IDENTIFICACION =:IDENTIFICACION";
+                $objQuery->setParameter("IDENTIFICACION", $strIdentificacion);
+                $objQueryCount->setParameter("IDENTIFICACION", $strIdentificacion);
+            }
+            if(!empty($intIdRestaurante))
+            {
+                $strWhere .= " AND IR.ID_RESTAURANTE =:ID_RESTAURANTE";
+                $objQuery->setParameter("ID_RESTAURANTE", $intIdRestaurante);
+                $objQueryCount->setParameter("ID_RESTAURANTE", $intIdRestaurante);
+            }
+            if(!empty($strTipoComida))
+            {
+                $strWhere  .= " AND ATC.ID_TIPO_COMIDA = :ID_TIPO_COMIDA";
+                $objQuery->setParameter("ID_TIPO_COMIDA", $strTipoComida);
+                $objQueryCount->setParameter("ID_TIPO_COMIDA", $strTipoComida);
+            }
+            $objRsmBuilder->addScalarResult('ID_RESTAURANTE', 'ID_RESTAURANTE', 'string');
+            $objRsmBuilder->addScalarResult('TIPO_IDENTIFICACION', 'TIPO_IDENTIFICACION', 'string');
+            $objRsmBuilder->addScalarResult('IDENTIFICACION', 'IDENTIFICACION', 'string');
+            $objRsmBuilder->addScalarResult('RAZON_SOCIAL', 'RAZON_SOCIAL', 'string');
+            $objRsmBuilder->addScalarResult('USUARIO_ID', 'USUARIO_ID', 'string');
+            $objRsmBuilder->addScalarResult('NOMBRE_COMERCIAL', 'NOMBRE_COMERCIAL', 'string');
+            $objRsmBuilder->addScalarResult('REPRESENTANTE_LEGAL', 'REPRESENTANTE_LEGAL', 'string');
+            $objRsmBuilder->addScalarResult('TIPO_COMIDA_ID', 'TIPO_COMIDA_ID', 'string');
+            $objRsmBuilder->addScalarResult('DESCRIPCION_TIPO_COMIDA', 'DESCRIPCION_TIPO_COMIDA', 'string');
+            $objRsmBuilder->addScalarResult('DIRECCION_TRIBUTARIO', 'DIRECCION_TRIBUTARIO', 'string');
+            $objRsmBuilder->addScalarResult('URL_CATALOGO', 'URL_CATALOGO', 'string');
+            $objRsmBuilder->addScalarResult('NUMERO_CONTACTO', 'NUMERO_CONTACTO', 'string');
+            $objRsmBuilder->addScalarResult('ESTADO', 'ESTADO', 'string');
+            $objRsmBuilderCount->addScalarResult('CANTIDAD', 'Cantidad', 'integer');
+            $strSql       = $strSelect.$strFrom.$strWhere;
+            $objQuery->setSQL($strSql);
+            $strSqlCount  = $strSelectCount.$strFrom.$strWhere;
+            $objQueryCount->setSQL($strSqlCount);
+            $arrayRestaurante['cantidad']   = $objQueryCount->getSingleScalarResult();
+            $arrayRestaurante['resultados'] = $objQuery->getResult();
+        }
+        catch(\Exception $ex)
+        {
+            $strMensajeError = $ex->getMessage();
+        }
+        $arrayRestaurante['error'] = $strMensajeError;
+        return $arrayRestaurante;
+    }
 }
