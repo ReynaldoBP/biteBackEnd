f26587719ffff25745f6271266de0be38b70f6b3
kbaque <kbaque@telconet.ec>
2019-09-10 10:48:52 -0500

    Web Services: se agrega controlador para las funciones post de la version web

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -18,6 +18,15 @@ class DefaultController extends Controller
             'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
         ]);
     }
+    /**
+     * Documentación para la función 'enviaCorreo'
+     * Método encargado de enviar correo según los parámetros recibidos.
+     * 
+     * @author Kevin Baque
+     * @version 1.0 26-08-2019
+     * 
+     * @return array  $objResponse
+     */
     public function enviaCorreo($arrayParametros)
     {
         $strAsunto        = $arrayParametros['strAsunto'] ? $arrayParametros['strAsunto']:'';
@@ -32,5 +41,35 @@ class DefaultController extends Controller
                                         ->setBody($strMensajeCorreo, 'text/html');
         $strRespuesta = $this->get('mailer')->send($objMessage);
         return $strRespuesta;
+    }
+    /**
+     * Documentación para la función 'subir_fichero'
+     * Método encargado de subir una imagen al servidor según los parámetros recibidos.
+     * 
+     * @author Kevin Baque
+     * @version 1.0 09-09-2019
+     * 
+     * @return array  $objResponse
+     */
+    public function subir_fichero($arrayParametros)
+    {
+        /*
+        ----------------------------------------------------------------------
+        jbermeo[INI]
+        ----------------------------------------------------------------------
+        */
+        $strRuta          = $arrayParametros['strRuta'] ? $arrayParametros['strRuta']:'';
+        $strNombreImagen  = $arrayParametros['strNombreImagen'] ? $arrayParametros['strNombreImagen']:'';
+        $strPeso          = $arrayParametros['strPeso'] ? $arrayParametros['strPeso']:'';
+        $strRespuesta     = 'llego';
+        $logger = $this->get('logger');
+        $logger->err('---------------------');
+        $logger->err($strRespuesta);
+        return $arrayParametros;
+        /*
+        ----------------------------------------------------------------------
+        jbermeo[FIN]
+        ----------------------------------------------------------------------
+        */
     }
 }
